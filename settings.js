560c3a54d5a56fb54b3948a49bd00ea45c7188fe
Zul-XYZD <162261728+Zul-XYZD@users.noreply.github.com>
2024-03-05 11:39:07 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -7,19 +7,19 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'CKWvEgbEhCG9z2D',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "Darwin-Bot"
+global.namaowner = "Zuldarwin-XD/MD"
 global.packname = namabot
 global.author = namaowner
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
+global.owner = ['6281396461615']
+global.pairingNumber = "6281396461615" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
